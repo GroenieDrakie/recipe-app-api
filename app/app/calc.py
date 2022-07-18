42d8c0451302145b051b2e351ede9c5871f956d9
GroenieDrakie <groenpvt@gmail.com>
2022-07-18 13:40:13 +0800

    Added db

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator function
+"""
+
+def add(x, y):
+	"""'Add x and y and retrun result."""
+	return x + y
+
+def subtract(x, y):
+	"""Subract x from y and return result."""
+	return y - x
