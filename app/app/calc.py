0d77403f9ffbf35b94cb246ae821792143f16a69
GroenieDrakie <groenpvt@gmail.com>
2022-07-18 16:53:35 +0800

    Fix indents and new lines and too many lines.

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,10 +4,10 @@ Calculator function
 
 
 def add(x, y):
-   """'Add x and y and retrun result."""
-   return x + y
+    """'Add x and y and retrun result."""
+    return x + y
 
 
 def subtract(x, y):
-   """Subract x from y and return result."""
-   return y - x
+    """Subract x from y and return result."""
+    return y - x
