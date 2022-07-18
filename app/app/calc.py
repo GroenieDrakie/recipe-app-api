82bd388499cbc27f6985b5477c361ad4bb654adb
GroenieDrakie <groenpvt@gmail.com>
2022-07-18 15:18:26 +0800

    wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator function
 """
 
+
 def add(x, y):
-	"""'Add x and y and retrun result."""
-	return x + y
+   """'Add x and y and retrun result."""
+   return x + y
+
 
 def subtract(x, y):
-	"""Subract x from y and return result."""
-	return y - x
+   """Subract x from y and return result."""
+   return y - x
